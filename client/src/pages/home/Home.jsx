c5ab03cbf2db869134ec72944318af4f0fe578c9
BegoCaZa <begoxcz@hotmail.com>
2025-06-04 20:44:24 +0200

    update

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,8 +1,22 @@
+import { useEffect, useState } from 'react';
+import { getAllData } from '../lib/utils/api.js';
+
 const Home = () => {
+	const [users, setUsers] = useState([]);
+
+	useEffect(() => {
+		getAllUsers(setUsers);
+	}, []);
 	return (
 		<>
 			<h2>HOME</h2>
 		</>
 	);
 };
+
+const getAllUsers = async setUsers => {
+	const users = await getAllData();
+	setUsers(users);
+};
+
 export default Home;
