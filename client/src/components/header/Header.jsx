950d937d73ad74db99e76c07e8cf0de68207c49e
BegoCaZa <begoxcz@hotmail.com>
2025-06-04 17:10:38 +0200

    update

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import {
+	StyledGeneralHeaderContainer,
+	StyledHeaderTitle
+} from './header.styles';
+
+const Header = () => {
+	return (
+		<StyledGeneralHeaderContainer>
+			<StyledHeaderTitle>Users Dashboard</StyledHeaderTitle>
+		</StyledGeneralHeaderContainer>
+	);
+};
+export default Header;
---
+++ client/src/components/header/header.styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const StyledGeneralHeaderContainer = styled.header`
+	width: 100%;
+	height: 100px;
+	display: flex;
+	align-items: center;
+	justify-content: flex-start;
+	background-color: #324e5a;
+	padding: 0 20px;
+`;
+
+export const StyledHeaderTitle = styled.h1`
+	margin: 0;
+	font-size: 24px;
+	color: white;
+	font-weight: 700;
+`;
