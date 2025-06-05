3c3125e1a9b547763e889429dddfc940f2a1e4aa
BegoCaZa <begoxcz@hotmail.com>
2025-06-05 17:08:19 +0200

    update

--- client/src/components/editingPage/EditingPage.jsx
+++ client/src/components/editingPage/EditingPage.jsx
@@ -1,8 +1,29 @@
-const EditingPage = () => {
+import { useForm } from 'react-hook-form';
+import {
+	StyledCheckbox,
+	StyledInputContainer,
+	StyledLabel
+} from './editingPage.styles';
+
+const EditingPage = ({ user, setUser, setEditingPage, userId }) => {
+	const { register } = useForm({
+		defaultValues: {
+			fullName: user.fullName,
+			email: user.email,
+			dateOfBirth: user.dateOfBirth,
+			phoneNumber: user.phoneNumber,
+			active: user.active
+		}
+	});
 	return (
-		<>
-			<h1>Editing Page</h1>
-		</>
+		<form>
+			<StyledInputContainer>
+				<StyledLabel>
+					<StyledCheckbox type='checkbox' {...register('active')} />
+					Active
+				</StyledLabel>
+			</StyledInputContainer>
+		</form>
 	);
 };
 export default EditingPage;
---
+++ client/src/components/editingPage/editingPage.styles.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import { COLORS } from '../../styles/colors';
+
+export const StyledInputContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+`;
+
+export const StyledLabel = styled.label`
+	font-weight: bold;
+	color: #333;
+`;
+
+export const StyledCheckbox = styled.input`
+	margin-right: 0.5rem;
+`;
